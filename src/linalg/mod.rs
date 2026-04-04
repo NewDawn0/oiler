9d75909370dfa3d76ab477fe16c4aed53f17fe76
NewDawn0 <newdawn.v0.0+git@gmail.com>
2026-04-04 14:55:31 +0200

    updadte mod

--- src/linalg/mod.rs
+++ src/linalg/mod.rs
@@ -1,4 +1,11 @@
-// pub mod linear_system;
+pub mod complex;
 pub mod matrix;
 pub mod traits;
 pub mod vector;
+
+pub mod prelude {
+    pub use crate::linalg::complex::*;
+    pub use crate::linalg::matrix::*;
+    pub use crate::linalg::traits::*;
+    pub use crate::linalg::vector::*;
+}
